3c8bae2fc80c1261bb1955f778fb1fb17b50817a
Sottocasa Michele <michelesottocasa1@gmail.com>
2022-10-28 16:38:37 +0200

    Updating db_Connector specifying table to use

--- db_Connector/db_Connector.php
+++ db_Connector/db_Connector.php
@@ -1,10 +1,11 @@
 <?php
-$servername = "spesaduezero.michelesottocasa.tech";
-$username = "spesa2.0";
-$password = "Spesa2.0";
+$db_servername = "spesaduezero.michelesottocasa.tech";
+$db_username = "spesa2.0";
+$db_password = "Spesa2.0";
+$db_table = "spesa2.0";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = mysqli_connect($db_servername, $db_username, $db_password, $db_table);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
