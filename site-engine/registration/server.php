daee984273b8d5108ab4d96a12fa217a07165fb6
Sottocasa Michele <michelesottocasa1@gmail.com>
2022-11-02 11:49:58 +0100

    Updating table names and columns names

--- site-engine/registration/server.php
+++ site-engine/registration/server.php
@@ -37,7 +37,7 @@ if (isset($_POST['reg_user'])) {
 
     // first check the database to make sure 
     // a user does not already exist with the same username and/or email
-    $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+    $user_check_query = "SELECT * FROM users WHERE UserName='$username' OR Mail='$email' LIMIT 1";
     $result = mysqli_query($db, $user_check_query);
     $user = mysqli_fetch_assoc($result);
 
@@ -55,7 +55,7 @@ if (isset($_POST['reg_user'])) {
     if (count($errors) == 0) {
         $password = md5($password_1); //encrypt the password before saving in the database
 
-        $query = "INSERT INTO users (username, email, password) 
+        $query = "INSERT INTO users (username, Mail, Password) 
   			  VALUES('$username', '$email', '$password')";
         mysqli_query($db, $query);
         $_SESSION['username'] = $username;
