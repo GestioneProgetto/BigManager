bb66be9dbb7f7aeae3ee3a60505c3477829498fb
andreocchiato <104194678+andreocchiato@users.noreply.github.com>
2022-11-04 18:14:32 +0100

    Ultimazione schermata registrati

--- grafica interfaccia login/Spesa2.0/login.js
+++ grafica interfaccia login/Spesa2.0/login.js
@@ -2,13 +2,16 @@ $(document).ready(function () {
     $("#bottone_accedi").click(function () {
         alert("Non ancora implementato");   
     });
+    $("#bottone_avanti").click(function () {
+        alert("Non ancora implementato");   
+    });
     $("#bottone_registrati").click(function () {
         $("#container_registrati").css("z-index", "100");
         $("#container_registrati").css("opacity", "100%");
     });
     $("#chiudi_registrati").click(function () {
         $("#container_registrati").css("opacity", "0%");
-        setTimeout(function(){$("#container_registrati").css("z-index", "-100");},500);
+        $("#container_registrati").css("z-index", "-100");
     });
     $(".bottone").mouseenter(function () {
         $(this).css("background-color", "#eee");
@@ -38,6 +41,34 @@ $(document).ready(function () {
             document.getElementById("accedi_password").type = "password";
             document.getElementById("occhio_g_accedi").hidden = false;
             document.getElementById("occhio_n_accedi").hidden = true;
+          }
+    });
+
+    $("#mostrapassregistrati1").click(function () {
+        if (document.getElementById("registrati_password1").type === "password") 
+        {
+            document.getElementById("registrati_password1").type = "text";
+            document.getElementById("occhio_g_registrati1").hidden = true;
+            document.getElementById("occhio_n_registrati1").hidden = false;
+          } else 
+          {
+            document.getElementById("registrati_password1").type = "password";
+            document.getElementById("occhio_g_registrati1").hidden = false;
+            document.getElementById("occhio_n_registrati1").hidden = true;
+          }
+    });
+
+    $("#mostrapassregistrati2").click(function () {
+        if (document.getElementById("registrati_password2").type === "password") 
+        {
+            document.getElementById("registrati_password2").type = "text";
+            document.getElementById("occhio_g_registrati2").hidden = true;
+            document.getElementById("occhio_n_registrati2").hidden = false;
+          } else 
+          {
+            document.getElementById("registrati_password2").type = "password";
+            document.getElementById("occhio_g_registrati2").hidden = false;
+            document.getElementById("occhio_n_registrati2").hidden = true;
           }
     });
 });
