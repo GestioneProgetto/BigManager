c0e8314e6ed0c6a6741871c657b2ef0346bfd668
Sottocasa Michele <34990942+MicheleSottocasa@users.noreply.github.com>
2022-11-17 12:28:17 +0100

    Update connectToDB.php

--- site-engine/core/functions/connectToDB.php
+++ site-engine/core/functions/connectToDB.php
@@ -1,8 +1,8 @@
 <?php
 
-$db_servername = "spesaduezero.michelesottocasa.tech";
-$db_username = "spesa2.0";
-$db_password = "Spesa2.0";
+$db_servername = "";
+$db_username = "";
+$db_password = "";
 
 // connect to the database
 $db = mysqli_connect($db_servername, $db_username, $db_password, 'spesa2.0');
