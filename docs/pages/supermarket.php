c91f6d671734c83ef2130b858dbb774f7e917231
andreocchiato <occhiato.andre@gmail.com>
2022-11-24 20:34:39 +0100

    bottoni admin arancioni
    cambio colore

--- docs/pages/supermarket.php
+++ docs/pages/supermarket.php
@@ -5,9 +5,9 @@ include_once 'core/index.php';
 <html>
 
 <head>
-    <link rel="stylesheet" href="/assets/style/admin.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="/assets/style/admin.css">
 </head>
 
 <body>
